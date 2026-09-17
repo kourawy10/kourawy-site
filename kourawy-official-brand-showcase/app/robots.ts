64dc014d6a2554074071a8fb411c530722f3230a
Kourawy <admin@kourawy.store>
2026-09-17 23:53:30 +0000

    Update robots.ts

--- kourawy-official-brand-showcase/app/robots.ts
+++ kourawy-official-brand-showcase/app/robots.ts
@@ -1,6 +1,9 @@
 import { MetadataRoute } from 'next';
 import { siteConfig } from '@/config/site';
 
+// Cette ligne indique à Next.js de générer ce fichier statiquement lors du build
+export const dynamic = 'force-static';
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
